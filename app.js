81f5010c6dce3471da981699cd4240189b52e8d8
estefania-luddeni <stefyluddeni@gmail.com>
2023-03-15 17:22:16 -0300

    route correction

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const mainRouter = require('./routers/main');
+const mainRouter = require('./routes/main');
 
 app.listen(3000, () => {
     console.log('Servidor funcionando');
